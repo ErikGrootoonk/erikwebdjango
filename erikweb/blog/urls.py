0ef5331076940347f1f8963972caa7b767e0d63e
ErikG <hgrootoonk@proton.me>
2026-01-02 09:49:55 +0100

    navbar added, manual css

--- erikweb/blog/urls.py
+++ erikweb/blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('about/', views.about, name='about'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- erikweb/blog/views.py
+++ erikweb/blog/views.py
@@ -8,3 +8,6 @@ def post_list(request):
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, published=True)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
